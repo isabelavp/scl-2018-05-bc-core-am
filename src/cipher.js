4ebeb5c9fb6f01f4ed55704d680c1e7682c5c8e9
isabelavp <veliz_isabel@hotmail.com>
2018-06-01 03:33:35 -0400

    cree el readme.md y añadi el src a otra carpeta

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,57 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+	
+  encode: (num,letter) => {
+    let result = "";
+   
+
+   
+     
+    for(var i = 0; i < letter.length; i++){
+    	
+    	let codeAnsii = letter.charCodeAt(i);
+
+    	
+    	//codigo ansii para letras mayusculas 
+    	if(codeAnsii >= 65 && codeAnsii < 91) { 
+    	result += String.fromCharCode((codeAnsii - 65 + parseInt(num) % 26) + 65);
+    	//codigo ansii para letras minusculas
+    	
+    	} else if(codeAnsii >= 97 && codeAnsii< 123) { 
+    	result += String.fromCharCode((codeAnsii -97 + parseInt(num) % 26) + 97);
+    	//codigo ansii para las demas
+    	        } else { 
+    	        	result += String.fromCharCode(codeAnsii);
+    	           	        }
+    	      
+     }
+ 
+     return result;
+  } 
+
+	
+  decode: (num,letter) => {
+    	let result = "";
+
+         for(var i = 0; i < letter.length; i++){
+    	
+    	let codeAnsii = letter(i).CharCodeAt();
+
+
+    	//codigo ansii para letras mayusculas 
+    	if(codeAnsii >= 65 && codeAnsii < 91) { 
+    	result += String.fromCharCode(codeAnsii - parseInt(num));
+    	//codigo ansii para letras minusculas
+    	
+    	} else if(codeAnsii >= 97 && codeAnsii< 123) { 
+    	result += String.CharCode(codeAnsii - parseInt(num));
+    	//codigo ansii para las demas
+    	        } else { 
+    	        	result += String.fromCharCode(codeAnsii);
+    	           	        }
+    	      
+       }
+     
+ 
+     return result;
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-/* Acá va tu código */
+window.onload = () => {
+
+    
+    const butEncode = document.getElementById("butEncode");
+
+    butEncode.addEventListener("click", () => {
+     let letter = document.getElementById("boxTextEncode").value;
+     let num = document.getElementById("key").value;
+     let result = window.cipher.encode(num,letter);
+          document.getElementById("boxTextTwo").innerHTML = result;
+    });
+     
+
+    const butDecode = document.getElementById("butDecode");
+
+    butDecode.addEventListener("click", () => {
+     let letter = document.getElementById("boxTextDecode").value;
+     let num = document.getElementById("key").value;
+     let result = window.cipher.decode(num,letter);
+          document.getElementById("boxTextTwo").innerHTML = result;
+
+    });
+    
+    const butClear = document.getElementById('butClear');
+
+    butClear.addEventListener("click", () => {
+        document.getElementById('boxTextEncode').value = '';
+        document.getElementById('boxTextDecode').value = '';
+        document.getElementById('boxTextTwo').value = '';
+        document.getElementById("key").value = 0;
+    }); 
+
+}
