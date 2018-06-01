b9ca877667cf0cda52c0829a3cd3346c0a2bcc5a
isabelavp <veliz_isabel@hotmail.com>
2018-06-01 03:37:52 -0400

    comentando todos los archivos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-window.onload = () => {
+window.onload = () => { //esta es la funcion de crear eventos con DOM
 
     
-    const butEncode = document.getElementById("butEncode");
+    const butEncode = document.getElementById("butEncode"); //variable const porque no se modifica, para boton de codigo
 
-    butEncode.addEventListener("click", () => {
+    butEncode.addEventListener("click", () => {  // uso de arrow function
      let letter = document.getElementById("boxTextEncode").value;
      let num = document.getElementById("key").value;
      let result = window.cipher.encode(num,letter);
@@ -11,9 +11,10 @@ window.onload = () => {
     });
      
 
-    const butDecode = document.getElementById("butDecode");
+    const butDecode = document.getElementById("butDecode"); //variable const porque no se modifica, para boton de decodificar
 
-    butDecode.addEventListener("click", () => {
+
+    butDecode.addEventListener("click", () => {  // uso de arrow function
      let letter = document.getElementById("boxTextDecode").value;
      let num = document.getElementById("key").value;
      let result = window.cipher.decode(num,letter);
@@ -21,9 +22,10 @@ window.onload = () => {
 
     });
     
-    const butClear = document.getElementById('butClear');
+    const butClear = document.getElementById('butClear');  //variable const porque no se modifica, para boton de limpieza
 
-    butClear.addEventListener("click", () => {
+
+    butClear.addEventListener("click", () => {  // uso de arrow function
         document.getElementById('boxTextEncode').value = '';
         document.getElementById('boxTextDecode').value = '';
         document.getElementById('boxTextTwo').value = '';
